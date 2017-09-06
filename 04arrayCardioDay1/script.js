f08c1d22542b0ae1efe964a57a217840b1f3b328
Charles Marks <clmarks15@gmail.com>
2017-09-06 11:13:33 -0400

    exercise entered

--- 04arrayCardioDay1/script.js
+++ 04arrayCardioDay1/script.js
@@ -21,6 +21,12 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter(function(inventor) {
+  if(inventor.year >= 1500 && inventor.year < 1600) {
+    return true; // keep it!
+  }
+});
+console.log(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
