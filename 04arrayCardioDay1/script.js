abad98827e8aa110b09f2636078a5c2306e09155
Charles Marks <clmarks15@gmail.com>
2017-09-06 15:22:55 -0400

    update

--- 04arrayCardioDay1/script.js
+++ 04arrayCardioDay1/script.js
@@ -9,8 +9,8 @@ const inventors = [
   { first:'Marie', last: 'Curie', year: 1867, passed: 1934},
   { first:'Johannes', last: 'Kepler', year: 1571, passed: 1630},
   { first:'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543},
-  { first:'Max', last: 'Planck', year: 1898, passed: 1947},
-  { first:'Katherine', last: 'Blodgett', year: 1879, passed: 1979},
+  { first:'Max', last: 'Planck', year: 1858, passed: 1947},
+  { first:'Katherine', last: 'Blodgett', year: 1898, passed: 1979},
   { first:'Ada', last: 'Lovelace', year: 1815, passed: 1852},
   { first:'Sarah E.', last: 'Goode', year: 1855, passed: 1905},
   { first:'Lise', last: 'Meitner', year: 1878, passed: 1968},
@@ -21,26 +21,85 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-const fifteen = inventors.filter(function(inventor) {
-  if(inventor.year >= 1500 && inventor.year < 1600) {
-    return true; // keep it!
-  }
-});
-console.log(fifteen);
+
+// **this will work, as will the shorter version below
+//const fifteen = inventors.filter(function(inventor) {
+//  if(inventor.year >= 1500 && inventor.year < 1600) {
+//    return true; // keep it!
+//  }
+//});
+
+const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600))
+console.table(fifteen);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
 
+// **this will work, as will the shorter version below
+//const fullNames = inventors.map(inventor => inventor.first + ' //' + inventor.last);
+//console.log(fullNames);
+
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullNames);
+
 // Array.prototype.sort()
 // 3. Sort inventors by birthdate, oldest to youngest
+
+// **this will work, as will the shorter version below
+//const ordered = inventors.sort(function(a, b) {
+//  if(a.year > b.year) {
+//    return 1;
+//  } else {
+//    return -1;
+//  }
+//});
+//
+//console.table(ordered)
+
+const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+console.table(ordered)
 
 // Array.prototype.reduce()
 // 4. How many years did the inventors live?
 
+// **this will work, as will the shorter version below
+//var totalYears = 0;
+//for(var i = 0; i < inventors.length; i++) {
+//  totalYears += inventors[i].year
+//}
+//console.log(totalYears)
+
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+console.log(totalYears);
+
 // 5. Sort the inventors by years lived
+
+// **this will work, as will the shorter version below
+//const oldest = inventors.sort(function(a, b) {
+//  const lastGuy = a.passed - a.year;
+//  const nextGuy = b.passed - b.year;
+//  if(lastGuy > nextGuy) {
+//    return -1;
+//  } else {
+//    return 1;
+//  }
+//});
+//console.table(oldest)
+
+const oldest = inventors.sort(function(a, b) {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+console.table(oldest)
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+
+
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
