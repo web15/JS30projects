f162da96c5b540471e8525c956633d50cd1a1368
Charles Marks <clmarks15@gmail.com>
2017-09-08 11:57:38 -0400

    complete

--- 05flexPanelsImageGallery/script.js
+++ 05flexPanelsImageGallery/script.js
@@ -1,2 +1,18 @@
 //alert("connected");
 
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if(e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
