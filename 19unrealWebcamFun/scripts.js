401d1fb50529f12dc27fb32dc8a50d1f1a151453
Charles Marks <clmarks15@gmail.com>
2017-09-21 15:19:00 -0400

    update

---
+++ 19unrealWebcamFun/scripts.js
@@ -0,0 +1,10 @@
+//alert('connected');
+
+const video =
+document.querySelector('.player');
+const canvas =
+document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip =
+document.querySelector('.strip');
+const snap = document.querySelector('.snap');
