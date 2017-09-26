4f5c81fbaad2b20a553af936a2c13b804993114d
Charles Marks <web15@users.noreply.github.com>
2017-09-26 16:01:19 -0400

    update

--- 19unrealWebcamFun/scripts.js
+++ 19unrealWebcamFun/scripts.js
@@ -76,6 +76,34 @@ function getVideo() {
     return pixels;
   }
 
-  function
+  function greenScreen(pixels)  {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) =>  {
+      levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4)  {
+      red = pixels.data[i + 0];
+      green = pixels.data [i + 1];
+      blue = pixels.data [i + 2];
+      alpha = pixels.data [i + 3];
+
+      if (red >= levels.rmin 
+        && green >= levels.gmin 
+        && blue >= levels.bmin 
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+          // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+
+    return pixels;
+  }
 
 getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
+
