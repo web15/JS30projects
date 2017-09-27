5e83d16b4dfb9ef2d979e57545cf83e03a397663
Charles Marks <web15@users.noreply.github.com>
2017-09-27 11:14:03 -0400

    complete

--- 19unrealWebcamFun/scripts.js
+++ 19unrealWebcamFun/scripts.js
@@ -60,18 +60,18 @@ function getVideo() {
 
   function redEffect(pixels)  {
     for(let i = 0; i < pivels.data.length; i+=4)  {
-      pixels.data[i + 0] = pixel.data[i + 0] + 200; //RED
-      pixels.data[i + 1] = pixel.data[i + 1] - 50; // GREEN
-      pixels.data[i + 2] = pixel.data[i + 2] * 0.5; // Blue
+      pixels.data[i + 0] = pixels.data[i + 0] + 200; //RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
     }
     return pixels;
   }
 
   function rgbSplit(pixels) {
     for(let i = 0; i < pixels.data.length; i+=4)  {
-      pixels.data[i - 150] = pixel.data[i + 0]; //RED
-      pixels.data[i + 500] = pixel.data[i + 1]; // GREEN
-      pixels.data[i - 550] = pixel.data[i + 2]; // Blue
+      pixels.data[i - 150] = pixels.data[i + 0]; //RED
+      pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+      pixels.data[i - 550] = pixels.data[i + 2]; // Blue
     }
     return pixels;
   }
