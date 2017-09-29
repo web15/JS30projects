4ce73133b424519d009b56395510d7ce6dd236b8
web15 <clmarks15@gmail.com>
2017-09-29 16:13:41 -0400

    bugg found

--- 22followAlongLinks/scripts.js
+++ 22followAlongLinks/scripts.js
@@ -7,13 +7,13 @@ const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.append(highlight);
 
-function highlightLink()  {
-  const linkCoords = this.getBoundingClientRect();
-  console.log(linkCoords);
-  highlight.style.width = `${linkCoords.width}px`; 
-  highlight.style.height = `${linkCoords.height}px`;
-  highlight.style.transform = `transform(${linkCoords.left}px, ${linkCoords.top}px)`;
-}
+//function highlightLink()  {
+//  const linkCoords = this.getBoundingClientRect();
+//  console.log(linkCoords);
+//  highlight.style.width = `${linkCoords.width}px`; 
+//  highlight.style.height = `${linkCoords.height}px`;
+//  highlight.style.transform = `transform($//{linkCoords.left}px, ${linkCoords.top}px)`;
+//}
 
 triggers.forEach(a => a. addEventListener('mouseenter', highlightLink));
 
