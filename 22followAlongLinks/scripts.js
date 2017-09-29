b6f6c2760f3525d779fd36e8f90af90753bf248d
web15 <clmarks15@gmail.com>
2017-09-29 15:43:35 -0400

    update looking for bugg

--- 22followAlongLinks/scripts.js
+++ 22followAlongLinks/scripts.js
@@ -1,3 +1,19 @@
 //alert('connected');
 
 //👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink()  {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  highlight.style.width = `${linkCoords.width}px`; 
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `transform(${linkCoords.left}px, ${linkCoords.top}px)`;
+}
+
+triggers.forEach(a => a. addEventListener('mouseenter', highlightLink));
+
