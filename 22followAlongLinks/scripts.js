c3f96b27224b723d4da541998a833ceab996f6e4
web15 <clmarks15@gmail.com>
2017-09-30 10:29:35 -0400

    completed

--- 22followAlongLinks/scripts.js
+++ 22followAlongLinks/scripts.js
@@ -7,13 +7,20 @@ const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.append(highlight);
 
-//function highlightLink()  {
-//  const linkCoords = this.getBoundingClientRect();
-//  console.log(linkCoords);
-//  highlight.style.width = `${linkCoords.width}px`; 
-//  highlight.style.height = `${linkCoords.height}px`;
-//  highlight.style.transform = `transform($//{linkCoords.left}px, ${linkCoords.top}px)`;
-//}
+function highlightLink()  {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
+  highlight.style.width = `${coords.width}px`; 
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
 
 triggers.forEach(a => a. addEventListener('mouseenter', highlightLink));
 
