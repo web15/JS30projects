fc54576a6510865524bb31ec0d0dd62b91b50e10
web15 <clmarks15@gmail.com>
2017-09-30 13:23:20 -0400

    ready for lesson

--- 23speechSynthesis/scripts.js
+++ 23speechSynthesis/scripts.js
@@ -1,2 +1,12 @@
-alert('connected');
+//alert('connected');
 
+
+    const msg = new SpeechSynthesisUtterance();
+    let voices = [];
+    const voicesDropdown =
+    document.querySelector('[name="voice"]');
+    const options = document.querySelectorAll('[type="range"], [name="text"]');
+    const speakButton = document.querySelector('#speak');
+    const stopButton = document.querySelector('#stop');
+
+    
